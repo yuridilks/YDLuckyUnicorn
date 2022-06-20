f04a7ea09ac8e56cc1eee915af940c7a67562e3f
yuridilks <107890787+yuridilks@users.noreply.github.com>
2022-06-21 09:25:42 +1200

    skeleton of the base code

---
+++ 00_base_v0.py
@@ -0,0 +1,17 @@
+"""
+base component for the game Lucky Unicorn
+v0 - skeleton setup
+
+author - Yuri Dilks
+CC YD 2022
+
+"""
+
+# import libraries below this line **********************
+
+
+# all functions below this line *************************
+
+
+# main routine below this line **************************
+print("Hello")
