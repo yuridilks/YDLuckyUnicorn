9ba57bf3de055ee17a0ab9e6f0ce8a7564da46e0
yuridilks <107890787+yuridilks@users.noreply.github.com>
2022-07-01 10:21:00 +1200

    yes / no function

---
+++ 02_yes_no_function_v1.py
@@ -0,0 +1,25 @@
+
+# Functions go here...
+def yes_no(question):
+    valid = False
+    while not valid:
+        response = input(question).lower()
+
+        if response == "yes" or response == "y":
+            response = "yes"
+            return response
+
+        elif response == "no" or response == "n":
+            response = "no"
+            return response
+
+        else:
+            print("Please answer yes / no")
+
+
+# Main Routine goes here...
+show_instructions = yes_no("Have you played the game before? ")
+print("You chose {}".format(show_instructions))
+print()
+having_fun = yes_no("Are you having fun? ")
+print("you said {} to having fun".format(having_fun))
